3c074611afe04f42e0630e14f87a0793b65d4a72
Sandra <72sandraloga@gmail.com>
2016-03-04 00:27:44 +0100

    modificaada la clase SupprtSystem para que no se pueda escribir nada despues del bye

--- SupportSystem.java
+++ SupportSystem.java
@@ -39,7 +39,7 @@ public class SupportSystem
         while(!finished) {
             String input = reader.getInput();
 
-            if(input.trim().toLowerCase().startsWith("bye")) {
+            if(input.toLowerCase().trim().equals("bye")) {
                 finished = true;
             }
             else {
